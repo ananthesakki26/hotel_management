c3c0db678de43bcea32b25a042d9c7583d80938e
ananthesakki26 <ananthesakki01@gmail.com>
2025-12-09 14:08:03 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB Connection
 mongoose.connect("mongodb://127.0.0.1:27017/hotelApp")
